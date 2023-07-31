013865ed0d81fcc1cb6ce681cdb581ff641ea93b
Fidaha <81554525+Fidaha@users.noreply.github.com>
2023-07-31 11:39:48 +0200

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class Rectangle : Polygon
+    {
+        private double width;
+        private double height;
+
+        public void SetWidth(double width)
+        {
+            this.width = width;
+        }
+
+        public void SetHeight(double height)
+        {
+            this.height = height;
+        }
+
+        public double GetWidth()
+        {
+            return width;
+        }
+
+        public double GetHeight()
+        {
+            return height;
+        }
+
+        public override double GetArea()
+        {
+            return width * height;
+        }
+
+
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class Square : Polygon
+    {
+
+        private double side;
+
+        public void SetSide(double side)
+        {
+            this.side = side;
+        }
+
+        public double GetSide()
+        {
+            return side;
+        }
+
+        public override double GetArea()
+        {
+            return side * side;
+        }
+
+    }
+}
